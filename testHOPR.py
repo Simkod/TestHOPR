efd1f7af5a267c8447dc04c27266b8364bc542bc
Daniel Simko <45272799+Simkod@users.noreply.github.com>
2023-04-26 10:35:06 +0200

    before implementing websockets

--- testHOPR.py
+++ testHOPR.py
@@ -1,28 +1,57 @@
 import asyncio
 import requests
 import json
+import time
+import websockets
 from string import Template
-from websockets.sync.client import connect
+#from websockets.sync.client import connect
 from prometheus_client.parser import text_string_to_metric_families
 
+ #Global Variables
+myApiAuthToken = '%th1s-IS-a-S3CR3T-ap1-PUSHING-b1ts-TO-you%'
+APIurlbase = Template('http://localhost:$APIport/api/v2/') 
+websocketAPIbaseurl = Template('ws://localhost:$APIport/api/v2/messages/websocket?apiToken=$AuthToken')
+
+APIPorts = {'node1': 13301,
+            'node2': 13302,
+            'node3': 13303,
+            'node4': 13304,
+            'node5': 13305}
+
+relevantMetricKeys = ['connect_counter_client_relayed_packets_total',
+                    'connect_counter_direct_packets_total',
+                    'connect_counter_server_relayed_packets_total',
+                    'core_counter_forwarded_messages_total',
+                    'core_counter_packets_total',
+                    'core_counter_received_messages_total',
+                    'core_counter_received_successful_acks_total',
+                    'core_counter_sent_acks_total',
+                    'core_counter_sent_messages_total',
+                    'core_ethereum_counter_indexer_announcements_total',
+                    'core_ethereum_counter_num_send_transactions_total',
+                    'core_gauge_num_incoming_channels',
+                    'core_gauge_num_outgoing_channels',
+                    'core_histogram_path_length_bucket']
+
+nodeHOPRAddresses = {}
 
 #Functions for communication
 def HTTPgetRequest(url, headers):
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    responseJson = response.json()
-    return responseJson
+    return response
 
 def HTTPpostRequest(url, headers, data):
     response = requests.post(url, json=data, headers=headers)
     response.raise_for_status()
-    responseJson = response.json()
-    return responseJson
+    return response
 
-def ConnectWebSocket(nodeAPIPort):
-    with connect(websocketAPIbaseurl.substitute(APIport=nodeAPIPort, AuthToken=myApiAuthToken)) as websocket: 
-        message = websocket.recv()
-        print(f"Received: {message}")
+def connectWebSocket(node):
+    nodeAPIPort = APIPorts['node5']
+    while True:
+        with websockets.connect(websocketAPIbaseurl.substitute(APIport=nodeAPIPort, AuthToken=myApiAuthToken)) as websocket:
+            message = websocket.recv()
+        #print(f"Received: {message}")
 
 #Prints
 def printResponseJson(jsonObject):
@@ -34,19 +63,6 @@ def printDict(dict):
     print('\n')
 
 #utilties
-def testChannelsForAllNodes():
-    for node, APIPort in APIPorts.items():
-        includingClosed='false'
-        url = APIurlbase.substitute(APIport=APIPort) + 'channels/?includingClosed=' + includingClosed
-        headers = {'accept': 'application/json',
-                   'x-auth-token': myApiAuthToken}
-
-        responseJson = HTTPgetRequest(url, headers) 
-
-        assert len(responseJson['incoming']) == (len(APIPorts)-1), node + 'is does not have an "incoming" channel from every node'
-        assert len(responseJson['outgoing']) == (len(APIPorts)-1), node + 'is does not have an "outgoing" channel to every node'
-    print('Incoming and outgoing channels - OK\n')
-
 def getNodeMetrics(nodeAPIPort):
     url = APIurlbase.substitute(APIport=nodeAPIPort) + 'node/metrics'
     headers = {'accept': 'application/json',
@@ -68,110 +84,103 @@ def getRelevantMetricsFor(node):
     return nodeMetricsDict
 
 def getNodeAddresses():
-    nodeHOPRAddresses = {}
     for node, APIPort in APIPorts.items():
         url = APIurlbase.substitute(APIport=APIPort) + 'account/addresses'
         headers = {'accept': 'application/json',
                    'x-auth-token': myApiAuthToken}
     
-        responseJson = HTTPgetRequest(url, headers)
+        response = HTTPgetRequest(url, headers)
+        responseJson = response.json()
         nodeHOPRAddresses[node] = responseJson['hopr']
     return nodeHOPRAddresses
 
-#TestCases
-def test_sendMessage_simple():
-    senderNode = 'node1'
-    receiverNode = 'node5'
-    
-    senderMetricsBefore = getRelevantMetricsFor(senderNode)
-    receiverMetricsBefore = getRelevantMetricsFor(receiverNode)
-
-    #To catch message at receiver Node
-    #ConnectWebSocket(APIPorts['node5'])
-
-    #send message from node 1 to 5 - not working, unauthorized 401. Waiting for Hopr answer. DO it Manually from Swagger until then
-    message = 'This is the message.'
+def sendMessage(senderNode, receiverNode, message, path, hops):
     recipientAddress = nodeHOPRAddresses[receiverNode]
-    path = '[]'
-    hops = 3
 
     url = APIurlbase.substitute(APIport=APIPorts[senderNode]) + 'messages'
     headers = {'accept': 'application/json',
-            'x-auth-token': myApiAuthToken,
-            'Content-Type': 'application/json'}
+               'x-auth-token': myApiAuthToken,
+               'Content-Type': 'application/json'}
     data = {'body': message,
             'recipient': recipientAddress,
             'path' : path,
-            'hops': str(hops)}
-    #responseJson = HTTPpostRequest(url, data, headers)
-    #printResponseJson(responseJson)
+            'hops': hops}
+    response = HTTPpostRequest(url, headers, data)
+    time.sleep(0.25)
+    return response
 
-    wait = input('Necessary pause because message sending API does not work. Send a message with the tesp parameters from the SwaggerUI and Press Enter to continue the test.\n')
+#Setup Tests
+def testChannelsForAllNodes():
+    for node, APIPort in APIPorts.items():
+        includingClosed='false'
+        url = APIurlbase.substitute(APIport=APIPort) + 'channels/?includingClosed=' + includingClosed
+        headers = {'accept': 'application/json',
+                   'x-auth-token': myApiAuthToken}
+
+        response = HTTPgetRequest(url, headers)
+        responseJson = response.json()
+
+        assert len(responseJson['incoming']) == (len(APIPorts)-1), node + 'is does not have an "incoming" channel from every node'
+        assert len(responseJson['outgoing']) == (len(APIPorts)-1), node + 'is does not have an "outgoing" channel to every node'
+    print('Incoming and outgoing channels - OK\n')
+
+def pingNodes():
+    for node, APIPort in APIPorts.items():
+        for HOPRaddress in nodeHOPRAddresses.values():
+            if HOPRaddress == nodeHOPRAddresses[node]:
+                continue
+            data = {'peerId': HOPRaddress}
+            url = APIurlbase.substitute(APIport=APIPort) + 'node/ping'
+            headers = {'accept': 'application/json',
+                    'x-auth-token': myApiAuthToken,
+                    'Content-Type': 'application/json'}
+            response = HTTPpostRequest(url, headers, data)
+    print('All node pinged from all nodes - OK')
+
+#TestCases
+def test_sendMessage(senderNode, receiverNode, message, path, hops):
+    senderMetricsBefore = getRelevantMetricsFor(senderNode)
+    receiverMetricsBefore = getRelevantMetricsFor(receiverNode)
+
+    #asyncio.get_event_loop().run_until_complete(connectWebSocket(receiverNode))
+    connectWebSocket(receiverNode)
+
+    #sign message
+
+    sendMessage(senderNode, receiverNode, message, path, hops)
 
     senderMetricsAfter = getRelevantMetricsFor(senderNode)
     receiverMetricsAfter = getRelevantMetricsFor(receiverNode)
 
-    #Received checker 1
-    print('Received msg count before: ' + str(receiverMetricsBefore['core_counter_received_messages_total']))
-    print('Received msg count after: ' + str(receiverMetricsAfter['core_counter_received_messages_total']))
     assert receiverMetricsBefore['core_counter_received_messages_total'] == receiverMetricsAfter['core_counter_received_messages_total']-1 , 'Message was not received'
 
-    #Received Checker 2
-    #Websocket
+    #Received Checker 2 - WebSocket
 
     #Send Check
-    #With Metrics
+        #With Metrics
+        #With response
 
     # Check tickets received for relaying - ???
 
-wait = input('Start the docker cluster by with     docker run --rm -d -p 8545:8545 -p 13301-13305:13301-13305 -p 18081-18085:18081-18085 -p 19091-19095:19091-19095 -p 19501-19505:19501-19505 --name pluto_cluster gcr.io/hoprassociation/hopr-pluto:1.92.7  and Press Enter to start the tests./n')
 
-#Global Variables
-myApiAuthToken = '%th1s-IS-a-S3CR3T-ap1-PUSHING-b1ts-TO-you%'
-APIurlbase = Template('http://localhost:$APIport/api/v2/') 
-websocketAPIbaseurl = Template('ws://localhost:$APIport/api/v2/messages/websocket?apiToken=$AuthToken')
+def main():
 
-APIPorts = {'node1': 13301,
-            'node2': 13302,
-            'node3': 13303,
-            'node4': 13304,
-            'node5': 13305}
+    #wait = input('Start the docker cluster by with     docker run --rm -d -p 8545:8545 -p 13301-13305:13301-13305 -p 18081-18085:18081-18085 -p 19091-19095:19091-19095 -p 19501-19505:19501-19505 --name pluto_cluster gcr.io/hoprassociation/hopr-pluto:1.92.7  and Press Enter to start the tests./n')
 
-relevantMetricKeys = ['connect_counter_client_relayed_packets_total',
-                       'connect_counter_direct_packets_total',
-                       'connect_counter_server_relayed_packets_total',
-                       'core_counter_forwarded_messages_total',
-                       'core_counter_packets_total',
-                       'core_counter_received_messages_total',
-                       'core_counter_received_successful_acks_total',
-                       'core_counter_sent_acks_total',
-                       'core_counter_sent_messages_total',
-                       'core_ethereum_counter_indexer_announcements_total',
-                       'core_ethereum_counter_num_send_transactions_total',
-                       'core_gauge_num_incoming_channels',
-                       'core_gauge_num_outgoing_channels',
-                       'core_histogram_path_length_bucket']
+    nodeHOPRAddresses = getNodeAddresses()
+    print('Node HOPR addresses:')
+    printDict(nodeHOPRAddresses)
 
+    pingNodes()
 
-nodeHOPRAddresses = getNodeAddresses()
-print('Node HOPR addresses:')
-printDict(nodeHOPRAddresses)
+    testChannelsForAllNodes()
 
-testChannelsForAllNodes()
+    senderNode = 'node1'
+    receiverNode = 'node5'
+    message = "This is the message."
+    path = []
+    hops = 1
+    test_sendMessage(senderNode, receiverNode, message, path, hops)
 
-test_sendMessage_simple()
-
-
-
-#ping nodes - unauthorized, not working
-""" for node, APIPort in APIPorts.items():
-    for HOPRaddress in nodeHOPRAddresses.values():
-        if HOPRaddress == nodeHOPRAddresses[node]:
-            continue
-        data = {'peerId': HOPRaddress}
-        url = APIurlbase.substitute(APIport=APIPort) + 'node/ping'
-        headers = {'accept': 'application/json',
-                   'x-auth-token': myApiAuthToken,
-                   'Content-Type': 'application/json'}
-        responseJson = HTTPpostRequest(url, data, headers)
-        print(responseJson) """
+if __name__ == "__main__":
+    main()
